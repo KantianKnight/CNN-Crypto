cc6654a8194970fb369210907d8c06a678382b83
Darren Chua <darrenchua314@gmail.com>
2024-11-20 02:38:48 +0800

    binance api streamlined

--- data.py
+++ data.py
@@ -1,2 +1,5 @@
 # Fetch data from (start date) to (end date) for BTC in the futures market
-# npx binance-fetch -d 2020-01-01 2020-12-31 -p spot -t klines -s dogeusd -i 1d -o data
+# npx binance-fetch -d 2010-01-01 2023-12-31 -p spot -t klines -s dogeusdt -i 1d -o rawdata
+
+# GENERAL
+# npx binance-historical download

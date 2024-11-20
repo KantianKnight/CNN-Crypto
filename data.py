6ef92df318a62fbf08bdaa1a1f0818b30a37c7e9
Darren Chua <darrenchua314@gmail.com>
2024-11-20 13:10:31 +0800

    clean

--- data.py
+++
@@ -1,5 +0,0 @@
-# Fetch data from (start date) to (end date) for BTC in the futures market
-# npx binance-fetch -d 2010-01-01 2023-12-31 -p spot -t klines -s dogeusdt -i 1d -o rawdata
-
-# GENERAL
-# npx binance-historical download
